582d2a942157f048fa3117c24ee2b18d523a5f88
Roberto Agustín Mejía Collazos <robertomejiacollazos@gmail.com>
2025-09-15 16:58:41 -0500

    docs: week 3,4 and 5

--- README.md
+++ README.md
@@ -10,4 +10,10 @@
 [![YouTube Video](https://img.youtube.com/vi/e2QKm8o3dWg/1.jpg)](https://www.youtube.com/watch?v=e2QKm8o3dWg)
 ### 2 - Clase Práctica
 [![YouTube Video](https://img.youtube.com/vi/hSnjkB2xn-0/1.jpg)](https://www.youtube.com/watch?v=hSnjkB2xn-0)
+### 3 - Clase Práctica
+[![YouTube Video](https://img.youtube.com/vi/zBk7u4w8xCA/1.jpg)](https://www.youtube.com/watch?v=zBk7u4w8xCA)
+### 4 - Clase Práctica
+[![YouTube Video](https://img.youtube.com/vi/79n17o9y3GI/1.jpg)](https://www.youtube.com/watch?v=79n17o9y3GI)
+### 5 - Clase Práctica
+[![YouTube Video](https://img.youtube.com/vi/x0hClrADQ1w/1.jpg)](https://www.youtube.com/watch?v=x0hClrADQ1w)
 
---
+++ semana-03/clase-03/src/components/ItemListContainer/ItemList.jsx
@@ -0,0 +1,46 @@
+
+import { useState } from "react";
+import Item from "./Item";
+
+const categories = {
+  PANADERIA: "Panadería y Cereales",
+  FRUTAS: "Frutas y Verduras",
+  LACTEOS: "Lácteos"
+};
+
+function ItemList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const filteredItems = selectedCategory
+    ? items.filter(item => item.categoria === selectedCategory)
+    : items;
+
+  return (
+    <div >
+      <div>
+        <button onClick={() => setSelectedCategory(categories.PANADERIA)}>
+          Panaderia y cereales
+        </button>
+        <button onClick={() => setSelectedCategory(categories.FRUTAS)}>
+          Frutas y verduras
+        </button>
+        <button onClick={() => setSelectedCategory(categories.LACTEOS)}>
+          Lacteos
+        </button>
+      </div>
+      <div className="container">
+
+      {filteredItems.map(item => (
+        <Item
+        key={item.id}
+        nombre={item.nombre}
+        precio={item.precio}
+        descripcion={item.descripcion}
+        />
+      ))}
+      </div>
+    </div>
+  );
+}
+
+export default ItemList;
